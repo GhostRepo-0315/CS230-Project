9c8cac5f708c655e2d7cca8a9b5ef48b9fc9fa3a
GhostRepo-0315 <langtiq@uci.edu>
2025-03-17 13:28:40 -0700

    Add files via upload

--- backend/rainbow_train.py
+++ backend/rainbow_train.py
@@ -4,30 +4,14 @@ from rainbow_agent import DQN
 import argparse
 import random
 
-seed_list = [40]
-
-NODE_SERVERS = [
-    "http://52.53.207.217:7001",
-    "http://18.144.171.222:7001",
-    "http://54.67.117.71:7001"
-]
-
-def seed_torch(seed):
-    torch.manual_seed(seed)
-    if torch.backends.cudnn.enabled:
-        torch.cuda.manual_seed(seed)
-        torch.backends.cudnn.benchmark = False
-        torch.backends.cudnn.deterministic = True
-
-
-
 class Runner:
     def __init__(self, args, number, seed):
         self.args = args
-        self.action_list = []
+
         self.number = number
         self.seed = seed
         self.env = UCLB()
+
         print("env name:", self.env.name)
         np.random.seed(seed)
         torch.manual_seed(seed)
@@ -84,8 +68,6 @@ class Runner:
             episode_steps = 0
             while not done:
                 action = self.agent.choose_action(state, epsilon=self.epsilon)
-                server = NODE_SERVERS[action]
-                self.action_list.append(server)
                 next_state, reward, done = self.env.step(action)
                 self.total_steps += 1
 
@@ -100,39 +82,70 @@ class Runner:
                 else:
                     terminal = False
 
-                self.replay_buffer.store_transition(state, action, reward, next_state, terminal,
-                                                    done)  # Store the transition
+                self.replay_buffer.store_transition(state, action, reward, next_state, terminal, done)  # Store the transition
                 state = next_state
 
                 if self.replay_buffer.current_size >= self.args.batch_size:
                     self.agent.learn(self.replay_buffer, self.total_steps)
 
+                # if self.total_steps % self.args.evaluate_freq == 0:
+                #     self.evaluate_policy()
+
+
+    # def evaluate_policy(self, ):
+    #     evaluate_reward = 0
+    #     self.agent.net.eval()
+    #     for _ in range(self.args.evaluate_times):
+    #         state = self.env_evaluate.reset()
+    #         done = False
+    #         episode_reward = 0
+    #         while not done:
+    #             action = self.agent.choose_action(state, epsilon=0)
+    #             next_state, reward, done = self.env_evaluate.step(action)
+    #             episode_reward += reward
+    #             state = next_state
+    #         evaluate_reward += episode_reward
+    #     self.agent.net.train()
+    #     evaluate_reward /= self.args.evaluate_times
+    #     self.evaluate_rewards.append(evaluate_reward)
+    #     print("total_steps:{} \t evaluate_reward:{} \t epsilon：{}".format(self.total_steps, evaluate_reward, self.epsilon))
+    #     self.writer.add_scalar('step_rewards_{}'.format(self.env_name), evaluate_reward, global_step=self.total_steps)
+
+
 if __name__ == '__main__':
+    seed_list = [37]
+
+    def seed_torch(seed):
+        torch.manual_seed(seed)
+        if torch.backends.cudnn.enabled:
+            torch.cuda.manual_seed(seed)
+            torch.backends.cudnn.benchmark = False
+            torch.backends.cudnn.deterministic = True
 
     episode_length = 8  # Number of steps / episode
-    episode_number = 1  # Number of episode to eval
+    episode_number = 400  # Number of episode to train
     steps = episode_number * episode_length  # Total step number
 
+    step_reward_matrix = np.zeros([len(seed_list), int(steps)])
+    step_delay_matrix = np.zeros([len(seed_list), int(steps)])
+    step_mad_matrix = np.zeros([len(seed_list), int(steps)])
+
     parser = argparse.ArgumentParser("Hyperparameter Setting for DQN")
-    parser.add_argument("--max_train_steps", type=int, default=int(steps), help=" Maximum number of evaling steps")
-    parser.add_argument("--evaluate_freq", type=float, default=1e3,
-                        help="Evaluate the policy every 'evaluate_freq' steps")
+    parser.add_argument("--max_train_steps", type=int, default=int(steps), help=" Maximum number of training steps")
+    parser.add_argument("--evaluate_freq", type=float, default=1e3, help="Evaluate the policy every 'evaluate_freq' steps")
     parser.add_argument("--evaluate_times", type=float, default=3, help="Evaluate times")
 
     parser.add_argument("--buffer_capacity", type=int, default=int(1e5), help="The maximum replay-buffer capacity ")
     parser.add_argument("--batch_size", type=int, default=256, help="batch size")
-    parser.add_argument("--hidden_dim", type=int, default=256,
-                        help="The number of neurons in hidden layers of the neural network")
+    parser.add_argument("--hidden_dim", type=int, default=256, help="The number of neurons in hidden layers of the neural network")
     parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate of actor")
     parser.add_argument("--gamma", type=float, default=0.99, help="Discount factor")
     parser.add_argument("--epsilon_init", type=float, default=0.5, help="Initial epsilon")
     parser.add_argument("--epsilon_min", type=float, default=0.1, help="Minimum epsilon")
-    parser.add_argument("--epsilon_decay_steps", type=int, default=int(1e5),
-                        help="How many steps before the epsilon decays to the minimum")
+    parser.add_argument("--epsilon_decay_steps", type=int, default=int(1e5), help="How many steps before the epsilon decays to the minimum")
     parser.add_argument("--tau", type=float, default=0.005, help="soft update the target network")
     parser.add_argument("--use_soft_update", type=bool, default=True, help="Whether to use soft update")
-    parser.add_argument("--target_update_freq", type=int, default=200,
-                        help="Update frequency of the target network(hard update)")
+    parser.add_argument("--target_update_freq", type=int, default=200, help="Update frequency of the target network(hard update)")
     parser.add_argument("--n_steps", type=int, default=5, help="n_steps")
     parser.add_argument("--alpha", type=float, default=0.6, help="PER parameter")
     parser.add_argument("--beta_init", type=float, default=0.4, help="Important sampling parameter in PER")
@@ -149,20 +162,25 @@ if __name__ == '__main__':
 
     for k in range(len(seed_list)):
         seed = seed_list[k]
-        print("Eval index:", k, "seed", seed)
+        print("Train index:", k, "seed", seed)
         np.random.seed(seed)
         random.seed(seed)
         seed_torch(seed)
+
         env_index = 0
 
         runner = Runner(args=args, number=1, seed=seed)
-
-        # load the model
-        runner.agent.net.load_state_dict = torch.load(f'rainbow_dqn_net_{runner.env.user_num}_users.pth')
-        runner.agent.target_net.load_state_dict = torch.load(f'rainbow_dqn_target_net_{runner.env.user_num}_users.pth')
+        print("algorithm:", runner.algorithm)
         runner.run()
 
+        step_reward_matrix[k, :] = np.array(runner.env.step_reward_list)
+        step_delay_matrix[k, :] = np.array(runner.env.step_delay_list)
+        step_mad_matrix[k, :] = np.array(runner.env.step_mad_list)
 
-        with open('action_list.txt', 'w') as file:
-            for item in runner.action_list:
-                file.write(f"{item}\n")
+
+    # save the model
+    torch.save(runner.agent.net.state_dict(), f'rainbow_dqn_net_{runner.env.user_num}_users.pth')
+    torch.save(runner.agent.target_net.state_dict(), f'rainbow_dqn_target_net_{runner.env.user_num}_users.pth')
+
+
+    runner.env.reset()
